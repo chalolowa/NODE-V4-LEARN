e50f1d4757d01a7416be44cd0ca8c38290ed0b0e
chalolowa <wangwelocha@students.uonbi.ac.ke>
2023-11-12 11:41:21 +0300

    On branch master  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   src/db.ts 	new file:   src/handlers/user.ts 	modified:   src/index.ts 	modified:   src/modules/auth.ts 	modified:   src/server.ts 	modified:   tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import app from './server';
+
 
 app.listen(3000, () => {
     console.log('Server successfully running on http//:localhost:3000');
